504cc7e2b715d32e7259ef40cb3aeb55299b7cdf
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-02-27 21:58:39 -0100

    new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import Navbar from "./components/Navbar"
-import Assignment from "./components/Assignment"
-import LogIn from "./components/LogIn"
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
+import UserSignup from "./components/Users/UserSignup"
+import Forgot from "./components/Layout/Forgot"
+import UserLogin from "./components/Users/UserLogin"
+import AdminSignup from "./components/Admin/AdminSignup"
+import AdminLogin from "./components/Admin/AdminLogin"
+import Home from "./components/Layout/Home"
+import Layout from "./components/Layout/Layout"
+import NotFound from "./components/Layout/NotFound"
+import UserDashboard from "./components/Users/UserDashboard"
+import UserLayout from "./components/Users/UserLayout"
+
 
 function App() {
   
 
   return (
     <>
-       <Routes>
-      <Route path="/" element={<Assignment/>}/>
-      <Route path="/login" element={<LogIn/>}/>
-      <Route path="/navbar" element={<Navbar/>}/>
-      
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home/>} />
+          
+        <Route path="*" element={<NotFound/>}/>
+
+        </Route>
+
+        <Route path="/user" element={<UserLayout/>}>
+        <Route path="/user" element={<Navigate to="/user/dashboard" />} />
+          <Route path="/user/dashboard" element={<UserDashboard/>} />
+        </Route>
+        
+        <Route path="/user/login" element={<UserLogin/>} />
+        <Route path="/user/signup" element={<UserSignup />} />
+        <Route path="/admin/signup" element={<AdminSignup />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/forgot" element={<Forgot/>}/>
+        
+      </Routes>
     </>
   )
 }
---
+++ src/components/Layout/Home.jsx
@@ -0,0 +1,21 @@
+import man from "../../assets/mon.png";
+import Button from "@mui/material/Button";
+// import { motion, useScroll } from "framer-motion";
+
+const Home = () => {
+ 
+  return (
+    <>
+      <main className="bg-gradient-to-r from-red-900 to-blue-950 lg:flex justify-between lg:px-20 lg:mt-20 w-full">
+        <div className="text-white break-words font-bold lg:mx-2 lg:mt-20 lg:visible invisible">
+          <h1 className="">
+            ADEBAYOZZ
+          </h1>
+        </div>
+      </main>
+    
+    </>
+  );
+};
+
+export default Home;
