238647a0f21e21862af29eed63aca62f2c76564b
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-03-06 09:58:57 -0100

    new commit

--- src/components/Users/UserNavbar.jsx
+++ src/components/Users/UserNavbar.jsx
@@ -12,48 +12,45 @@ const UserNavbar = () => {
         <main className='bg-yellow-500'>
             <div className="flex h-screen ">
                 
-                <div className={`w-24 bg-red-700 transform transition-transform duration-300 ${isOpen ? '' : '-translate-x-full'} md:block md:w-52`}>
+                <div className={`w-24 bg-red-500 transform transition-transform duration-1000 ${isOpen ? '' : '-translate-x-full'} md:block md:w-56`}>
                     <div className="p-2">
-                        <h1 className=" text-lg font-semibold text-cyan-600 shadow-md">BESTWAY COLLEGE</h1>
+                        <h1 className=" font-extrabold text-neutral-950 shadow-lg shadow-neutral-950 lg:text-base sm:text-sm">BESTWAY COLLEGE</h1>
                     </div>
                   
                     <nav className="mt-16">
-                        <ul className="text-white shadow-md shadow-slate-700 my-20">
-                            <li className="py-2 px-4 mt-10">
-                                <a href="#" className="text-cyan-600 shadow-md bg-red-700">DASH</a>
+                        <ul className="text-white shadow-lg ">
+                            <li className="lg:py-2 px-4 mt-32">
+                                <a href="#" className="font-semibold text-neutral-950 shadow-lg shadow-neutral-950 bg-red-500 lg:text-base sm:text-sm">DASH</a>
                             </li>
-                            <li className="py-2 px-4 mt-3">
-                                <a href="#" className="text-cyan-600 shadow-md bg-red-700">ADMISS</a>
+                            <li className="py-2 px-4 mt-12">
+                                <a href="#" className="font-semibold text-neutral-950 shadow-lg shadow-neutral-950 bg-red-500 lg:text-base sm:text-sm">ADMISS</a>
                             </li>
-                            <li className="py-2 px-4 mt-3">
-                                <a href="#" className="text-cyan-600 shadow-md bg-red-700">STATUS</a>
+                            <li className="py-2 px-4 mt-12">
+                                <a href="#" className="font-semibold text-neutral-950 shadow-lg shadow-neutral-950 bg-red-500 lg:text-base sm:text-sm">STATUS</a>
                             </li>
-                            <li className="py-2 px-4 mt-3">
-                                <a href="#" className="text-cyan-600 shadow-md bg-red-700">PROFILE</a>
+                            <li className="py-2 px-4 mt-12">
+                                <a href="#" className="font-semibold text-neutral-950 shadow-lg shadow-neutral-950 bg-red-500 lg:text-base sm:text-sm">PROFILE</a>
                             </li>
-                            <li className="py-2 px-4 mt-3">
-                                <a href="#" className="text-cyan-600 shadow-md bg-red-700">CONTACT</a>
+                            <li className="py-2 px-4 mt-12">
+                                <a href="#" className="font-semibold text-neutral-950 shadow-lg shadow-neutral-950 bg-red-500 lg:text-base sm:text-sm">CONTACT</a>
                             </li>
-                            <li className="py-2 px-4 mt-3">
-                                <a href="#" className="text-cyan-600 shadow-md bg-red-700">LOGOUT</a>
+                            <li className="py-2 px-4 mt-12">
+                                <a href="#" className="font-semibold text-neutral-950 shadow-lg shadow-neutral-950 bg-red-500 lg:text-base sm:text-sm">LOGOUT</a>
                             </li>
                         </ul>
                     </nav>
                 </div>
-                {/* Main Content */}
-                <div className={`flex-1 ${isOpen ? 'ml-4' : ''}`}>
-                    {/* Toggle Button (visible on small screens) */}
-                    <div className="flex justify-end p-4 md:hidden ">
-                        <button onClick={toggleSidebar} className="text-red-600 focus:outline-none">
+                <div className={`flex-1 ${isOpen ? 'ml-16' : ''}`}>
+                    <div className="flex justify-end p-4 md: ">
+                        <button onClick={toggleSidebar} className="text-neutral-950 focus:outline-none">
                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                             </svg>
                         </button>
                     </div>
-                    {/* Page Content */}
-                    <div className="p-0">
-                        <h1 className="text-lg font-semibold text-cyan-600">Main Content</h1>
-                        <p className='text-cyan-800'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut tortor urna. Vivamus interdum ipsum eu metus ullamcorper, vel lacinia mi volutpat.</p>
+                    <div className="">
+                        <h1 className="text-lg font-semibold text-neutral-950">Main Content</h1>
+                        <p className='text-neutral-950'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut tortor urna. Vivamus interdum ipsum eu metus ullamcorper, vel lacinia mi volutpat.</p>
                     </div>
                 </div>
             </div>
